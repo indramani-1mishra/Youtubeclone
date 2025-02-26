ac61cc487b719e0110ace9b87c47719afe102722
RAJ <rajmishra.ydd@gmail.com>
2025-02-26 20:03:16 +0530

    response searchbar and functionlity added in my project

--- src/component/Header/HeaderforMobileScreen/HeaderforMobile.jsx
+++ src/component/Header/HeaderforMobileScreen/HeaderforMobile.jsx
@@ -1,25 +1,56 @@
+import { useState } from 'react';
 import './HeaderForMobile.css';
 import { IoMdNotificationsOutline } from 'react-icons/io';
 import { IoSearchOutline } from "react-icons/io5";
 import { MdConnectedTv } from "react-icons/md";
-export default function HeaderforMobile() {
+import SearchBar from '../Searchbar/SearchBar';
+
+import { FaArrowLeft } from "react-icons/fa6";
+import { FaMicrophone } from 'react-icons/fa';
+
+export default function HeaderForMobile() {
     const youtubeImage = "https://t3.ftcdn.net/jpg/06/34/31/96/360_F_634319630_txtgmPLEEQ8o4zaxec2WKrLWUBqdBBQn.jpg";
-  return (
-    <div className='HeaderContainerForMobile'>
-      <div className="YoutubeImageContainer2">
-        <img src={youtubeImage} alt=" youtube image"/>
-      </div>
-      <div className='Containerforsmall'>
-       <div className="notificationConatainer2">
-            <IoMdNotificationsOutline fontSize="30px" />
-            </div>
-            <div className="notificationConatainer2">
-            <MdConnectedTv  fontSize="30px" />
-            </div>
-            <div className="notificationConatainer2">
-            <IoSearchOutline  fontSize="30px" />
-            </div>
-</div>
-    </div>
-  )
+    const [isTouch, SetTouch] = useState(true);
+
+    const onclickHandler = () => {
+        window.location.reload();
+    }
+
+    const onTouchHandler = () => {
+        SetTouch(false);
+    }
+    const closeSearchBar=()=>
+    {
+        SetTouch(true);
+    }
+
+    return (
+        <div className='HeaderContainerForMobile'>
+            {isTouch ? (
+                <>
+                    <div className="YoutubeImageContainer2" onTouchStart={onclickHandler}>
+                        <img src={youtubeImage} alt="youtube image" />
+                    </div>
+                    <div className='Containerforsmall'>
+                        <div className="notificationConatainer2">
+                            <IoMdNotificationsOutline fontSize="30px" />
+                        </div>
+                        <div className="notificationConatainer2">
+                            <MdConnectedTv fontSize="30px" />
+                        </div>
+                        <div className="notificationConatainer2" onTouchStart={onTouchHandler}>
+                            <IoSearchOutline fontSize="30px" />
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <div className='searchbar3'>
+                <FaArrowLeft fontSize="32px" onClick={closeSearchBar} color='gray' />
+                <SearchBar />
+                <FaMicrophone fontSize="32px"  color='gray'/>
+
+                </div>
+            )}
+        </div>
+    );
 }
