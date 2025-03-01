1aa26bf133429373175404482379e1895de3c9b3
RAJ <rajmishra.ydd@gmail.com>
2025-03-01 11:46:12 +0530

    profile icon added in my youtube clone

--- src/component/Header/Profile/Profile.jsx
+++ src/component/Header/Profile/Profile.jsx
@@ -3,6 +3,7 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 import { Avatar } from "@mui/material";
 import './Profile.css';
 export default function Profile() {
+  const profile= "https://scontent.fvns1-2.fna.fbcdn.net/v/t39.30808-6/438256407_1169122724264056_5310981140638874357_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=xemo5HVHSScQ7kNvgG2i2gr&_nc_oc=AdgKHjPntqI3WdrZ6a7LfdaA4VNOPlMdiVWYBsuDb0-APJXmjlgc_grqhpuJWjZZG18ObRNq0fat4bCBVf03DtpV&_nc_zt=23&_nc_ht=scontent.fvns1-2.fna&_nc_gid=AeRCKA1PZ_JB4v7VdzF76qM&oh=00_AYDcItibJQBsC6iOxxG2hv8_Vb-x2H8jkegzbnnVsbwehw&oe=67C864FF";
     
   return (
     <div className="ProfileIconContainer">
@@ -16,7 +17,7 @@ export default function Profile() {
       <div className="ProfileImageConatainer">
         <Avatar 
           alt="Profile Picture"
-          src="raj.JPG"// ✅ Local Image now works!
+          src={profile}// ✅ Local Image now works!
           sx={{ width: 40, height: 40, cursor: "pointer" }}
         />
       </div>
--- src/component/Sidebar/Foter/Footer.jsx
+++ src/component/Sidebar/Foter/Footer.jsx
@@ -7,9 +7,12 @@ import { useContext } from "react";
 import OpenCloseContext from "../../../Context/OpenCloseContext/OpenCloseContext";
 
 import './Footer.css';
+import { Avatar } from "@mui/material";
 export default function Footer() {
  // const data = footerData();  // ✅ Custom hook ka naam change kiya
  const { ShortButton, setShortButton } = useContext(OpenCloseContext);
+ const profile= "https://scontent.fvns1-2.fna.fbcdn.net/v/t39.30808-6/438256407_1169122724264056_5310981140638874357_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=xemo5HVHSScQ7kNvgG2i2gr&_nc_oc=AdgKHjPntqI3WdrZ6a7LfdaA4VNOPlMdiVWYBsuDb0-APJXmjlgc_grqhpuJWjZZG18ObRNq0fat4bCBVf03DtpV&_nc_zt=23&_nc_ht=scontent.fvns1-2.fna&_nc_gid=AeRCKA1PZ_JB4v7VdzF76qM&oh=00_AYDcItibJQBsC6iOxxG2hv8_Vb-x2H8jkegzbnnVsbwehw&oe=67C864FF";
+    
 
   return (
     <>
@@ -35,7 +38,12 @@ export default function Footer() {
           <span>subscriptions</span>
         </div> 
         <div>
-          <FaUserCircle  fontSize="32px"/>
+         {/**<FaUserCircle  fontSize="32px"/> */}
+         <Avatar 
+          alt="Profile Picture"
+          src={profile}// ✅ Local Image now works!
+          sx={{ width: 20, height: 20, cursor: "pointer" }}
+        />
           <span>profile</span>
         </div> 
        </div>
